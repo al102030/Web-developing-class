3240af73c1e0c6ee8b3248c78de066260a88c6ba
Ali Darvishi <ali.darvishi.k@gmail.com>
2023-09-27 21:08:14 +0330

    Add some note for python crash course.(Add dictionary for question Eleven.)

--- app.py
+++ app.py
@@ -247,3 +247,32 @@
 
 
 # ======================================classes==========================
+
+phonebook = {
+    "John Doe": "09111234562",
+    "Jane Smith": "09359876543",
+    "Alice Johnson": "09114567894",
+    "Bob Thompson": "09357891233",
+    "Emily Davis": "09112461358",
+    "Michael Wilson": "09351352496",
+    "Sarah Anderson": "09115793507",
+    "David Clark": "09353575789",
+    "Olivia Baker": "09118246850",
+    "Daniel Green": "09356808244",
+    "Sophia King": "09114682436",
+    "William Lee": "09352464681",
+    "Mia Miller": "09113575799",
+    "Joseph Hill": "09355793576",
+    "Grace Adams": "09112461357",
+    "Andrew Parker": "09351352465",
+    "Elizabeth Stewart": "09115793570",
+    "Matthew Turner": "09353575749",
+    "Ava Bennett": "09116808234",
+    "Anthony Morgan": "09358246820",
+    "Chloe Cook": "09112464628",
+    "Christopher Reed": "09354682426",
+    "Victoria Scott": "09115793574",
+    "Ryan Phillips": "09353575795",
+    "Ella Young": "09111352466",
+    "Benjamin Hall": "09352461359",
+}
